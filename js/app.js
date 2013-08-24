4b65761955c23fc7213f1df75f9b7128ee1f2e31
Michael Swartz <mswartz@gmail.com>
2013-08-24 09:37:21 -0400

    playing with image splitting some more

--- js/app.js
+++ js/app.js
@@ -10,15 +10,25 @@ $(document).ready(function(){
 
 	//add the img to the space
 	$(document).on('click', '.add_img', function(){
-		$(this).parent().html("<div class='img_container'><div class='img_layout'></div><img src='img/image.jpg'/></div>");
+		$(this).parent().html("<div class='img_container'><div class='img_layout'><a class='img_vert' href='#'>||</a>&nbsp;<a class='img_horiz' href='#'>=</a></div><img src='img/image.jpg'/></div>");
+		return false;
 	});
 
-	$(document).on('click', '.img_layout', function(){
-		$(this).parent().css({
+	$(document).on('click', '.img_vert', function(){
+		var h = $(this).closest('.img_container').height();
+		$(this).closest('.img_container').css({
 			"width": "50%",
 			"float": "left"
 		});
-		$(this).closest('.space').append("<div class='img_container' style='width:50%; float:left;'><div class='img_layout'></div><img src='img/image.jpg'/></div>");
+		$(this).closest('.space').append("<div class='img_container' style='width:50%; float:left;'><div class='img_layout'><a class='img_vert' href='#'>||</a>&nbsp;<a class='img_horiz' href='#'>=</a></div><img src='img/image.jpg'/></div>");
+	});
+
+	$(document).on('click', '.img_horiz', function(){
+		var h = $(this).closest('.img_container').height();
+		$(this).closest('.img_container').css({
+			"height": h/2
+		});
+		$(this).closest('.space').append("<div class='img_container' style='height:"+h/2+"px; float: left;'><div class='img_layout'></div><img src='img/image.jpg'/></div>");
 	});
 
 });
