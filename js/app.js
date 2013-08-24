c11d954bb527e70ca75a71a808ded01dd6c95944
Michael Swartz <mswartz@gmail.com>
2013-08-23 23:49:36 -0400

    implementing img cleaving/cloning, interesting results so far

--- js/app.js
+++ js/app.js
@@ -2,13 +2,23 @@ var spaceFrag = '<div class="space" contenteditable="false"><a href="#" class="a
 
 $(document).ready(function(){
 
+
 	//function for turning [img] into spaces
 	$('body').keypress(function(){
 		$("p:contains('[img]')").replaceWith(spaceFrag);
 	});
 
+	//add the img to the space
 	$(document).on('click', '.add_img', function(){
-		$(this).parent().html("<img src='img/image.jpg'/>");
+		$(this).parent().html("<div class='img_container'><div class='img_layout'></div><img src='img/image.jpg'/></div>");
+	});
+
+	$(document).on('click', '.img_layout', function(){
+		$(this).parent().css({
+			"width": "50%",
+			"float": "left"
+		});
+		$(this).closest('.space').append("<div class='img_container' style='width:50%; float:left;'><div class='img_layout'></div><img src='img/image.jpg'/></div>");
 	});
 
 });
